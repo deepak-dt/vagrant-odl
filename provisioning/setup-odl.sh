0daeeb31d1c37c3879c51123260018a3104ed8cf
deepak-dt <deepak.dt@gmail.com>
2017-07-28 12:18:36 +0530

    Update setup-odl.sh

--- provisioning/setup-odl.sh
+++ provisioning/setup-odl.sh
@@ -16,5 +16,6 @@ sudo chown -R vagrant:vagrant distribution-karaf-0.5.4-Boron-SR4
 # sudo apt-get -y install oracle-java8-installer 
 # cd distribution-karaf-0.5.4-Boron-SR4/bin
 # Run ./karaf
-# feature:install odl-aaa-authn odl-restconf odl-mdsal-apidocs odl-ovsdb-openstack odl-dlux-core odl-neutron-northbound-api odl-restconf-all odl-dlux-all odl-l2switch-all
+# feature:install odl-aaa-authn odl-restconf odl-mdsal-apidocs odl-ovsdb-openstack odl-dlux-all odl-l2switch-all
+# NOT- odl-neutron-northbound-api odl-restconf-all odl-dlux-core
 # Open http://localhost:8181/index.html#/topology to see if everything works fine
