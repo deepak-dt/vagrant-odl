34f78cc7b686bc8a9a0510dd182793fc1ba91822
Anonymous <anonymous@nowhere.com>
2017-07-28 12:02:33 +0530

    changes in startup script

--- provisioning/setup-odl.sh
+++ provisioning/setup-odl.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 # Steps to install and configure ODL-boron
-sudo apt-get install wget
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+sudo apt-get -y install wget
+sudo apt-get -y install default-jre
+sudo apt-get -y install default-jdk
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get -y install oracle-java8-installer
 wget https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/0.5.4-Boron-SR4/distribution-karaf-0.5.4-Boron-SR4.tar.gz
 tar -xvf distribution-karaf-0.5.4-Boron-SR4.tar.gz
+sudo chown -R vagrant:vagrant distribution-karaf-0.5.4-Boron-SR4
 
 # Steps to start Karaf shell
+# Bewlo command requires manual intervention
+# sudo apt-get -y install oracle-java8-installer 
 # cd distribution-karaf-0.5.4-Boron-SR4/bin
 # Run ./karaf
 # feature:install odl-aaa-authn odl-restconf odl-mdsal-apidocs odl-ovsdb-openstack odl-dlux-core odl-neutron-northbound-api odl-restconf-all odl-dlux-all odl-l2switch-all
